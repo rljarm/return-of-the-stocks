a94c36703e36c7cd151d1d715fdfb23d3c718c65
Dick <rljarm@gmail.com>
2015-04-22 17:07:41 -0700

    api installed

--- client/index.js
+++ client/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('day-trader', ['firebase'])
+.run(['$rootScope', '$window', function($rootScope, $window) {
+  $rootScope.fbRoot = new $window.Firebase('https://return-of-stocks.firebaseio.com/');
+}])
+.controller('master', ['$scope', '$firebaseObject', '$firebaseArray', '$http', function($scope, $firebaseObject, $firebaseArray, $http){
+  $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=AAPL&callback=JSON_CALLBACK').then(function(response){
+console.info(response.data.LastPrice);
+});
+}]);
